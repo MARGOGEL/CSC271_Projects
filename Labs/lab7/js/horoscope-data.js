431e5905d846f2c60f8952701f0ee8b8f6411b32
MARGOGEL <104650794+MARGOGEL@users.noreply.github.com>
2024-10-26 15:34:31 -0400

    r

---
+++ Labs/lab7/js/horoscope-data.js
@@ -0,0 +1,25 @@
+var mySign = "Scorpio";
+var bmonth = "November";
+var day= 16;
+var MyluckyNum = 20.0;
+var hDescription= "Description";
+var horoscopeBool= false;
+
+
+var sign = document.getElementById('sign');
+sign.textContent = mySign;
+
+var birthDay=document.getElementsByClassName('birthday');
+birthDay[0].textContent+= bmonth + ' ' + day  + "th"
+
+var numlucky = document.getElementsByClassName('luckyNum');
+numlucky[0].textContent+= MyluckyNum
+
+
+
+
+
+
+
+
+
