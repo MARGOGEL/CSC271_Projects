00b447d8a1b063ebb6b651cfb2b562e2de431d09
MARGOGEL <104650794+MARGOGEL@users.noreply.github.com>
2024-10-26 17:43:45 -0400

    r

--- product_site/js/simple_script.js
+++ product_site/js/simple_script.js
@@ -11,6 +11,12 @@ function calculateHomelyFee() {
     for (var i = 0; i < feeClassElements.length; i++) {
         feeClassElements[i].textContent = "Based on a home price of $" + homePrice;
     }
+
+    var tagElements = document.getElementsByTagName("p");
+    if (tagElements.length > 0) {
+        tagElements[0].innerHTML = `<strong>Note:</strong> This fee is only an estimate.`;
+    }
+
     var feeHighlight = document.querySelector(".fee-highlight");
     if (feeHighlight) {
         feeHighlight.innerHTML = `<em>Fee Calculated:</em> $${homelyFee.toFixed(2)}`;
